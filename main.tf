58a91a0ea0ce5e74360dbbbe23cfb48220574b86
Paridhi <paridhiag.91@gmail.com>
2025-03-12 07:04:05 +0000

    commit

--- main.tf
+++ main.tf
@@ -65,17 +65,17 @@ resource "aws_lb" "test" {
   }
 }
 
-resource "aws_db_instance" "Paridhi_rds" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
-}
+# resource "aws_db_instance" "Paridhi_rds" {
+#   allocated_storage    = 10
+#   db_name              = "mydb"
+#   engine               = "mysql"
+#   engine_version       = "8.0"
+#   instance_class       = "db.t3.micro"
+#   username             = "foo"
+#   password             = "foobarbaz"
+#   parameter_group_name = "default.mysql8.0"
+#   skip_final_snapshot  = true
+# }
 
 
 resource "aws_vpc" "main" {
